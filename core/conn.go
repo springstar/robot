747a5327f5dac64226c4b6f9d2cba7072bd2bf38
wuchunxin <wuyutong0908@gmail.com>
2023-05-06 15:45:30 +0800

    packet

--- core/conn.go
+++ core/conn.go
@@ -1,7 +1,12 @@
 package core
 
+import (
+	
+)
+
 type NetConnection interface {
 	Connect(addr string) error
+	Write(p []byte) (n int, err error)
 }
 
 
--- core/ws.go
+++ core/ws.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+
 	"fmt"
 	"time"
 	"context"
@@ -8,24 +9,32 @@ import (
 )
 
 type WebSockConn struct {
-
+	c *websocket.Conn
 }
 
 func NewWsConnection() *WebSockConn {
 	return &WebSockConn{
-
 	}
 }
 
-func (conn *WebSockConn) Connect(addr string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+func (wsc *WebSockConn) Connect(addr string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
 	defer cancel()
-	
-	c, _, err := websocket.Dial(ctx, addr, nil)
+	var err error
+	wsc.c, _, err = websocket.Dial(ctx, addr, nil)
 	if err != nil {
 		fmt.Print("connect err ", err)
 	}
-	defer c.Close(websocket.StatusInternalError, "the sky is falling")
+
 	return nil
 }
 
+func (wsc *WebSockConn) Write(p []byte) (n int, err error) {
+	n = len(p)
+	err = wsc.c.Write(context.Background(), websocket.MessageBinary, p)
+	if (err != nil) {
+		fmt.Println(err)
+		return 0, err
+	}
+	return n, nil
+}
