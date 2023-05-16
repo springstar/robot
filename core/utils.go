158df890630732cd4ac78e18b862ef9ee682f504
wuchunxin <wuyutong0908@gmail.com>
2023-05-16 19:01:25 +0800

    add utils

--- core/utils.go
+++ core/utils.go
@@ -3,27 +3,57 @@ package core
 import (
 	"fmt"
 	"reflect"
-	"errors"
 )
 
 func SetField(obj interface{}, name string, value interface{}) error {
-    structValue := reflect.ValueOf(obj).Elem()
-    structFieldValue := structValue.FieldByName(name)
 
-    if !structFieldValue.IsValid() {
+    structValue := reflect.ValueOf(obj).Elem()
+    fieldVal := structValue.FieldByName(name)
+
+    if !fieldVal.IsValid() {
         return fmt.Errorf("No such field: %s in obj", name)
     }
 
-    if !structFieldValue.CanSet() {
+    if !fieldVal.CanSet() {
         return fmt.Errorf("Cannot set %s field value", name)
     }
 
-    structFieldType := structFieldValue.Type()
     val := reflect.ValueOf(value)
-    if structFieldType != val.Type() {
-        return errors.New("Provided value type didn't match obj field type")
+
+    if fieldVal.Type() != val.Type() {
+
+        if m,ok := value.(map[string]interface{}); ok {
+
+            // if field value is struct
+            if fieldVal.Kind() == reflect.Struct {
+                return FillStruct(m, fieldVal.Addr().Interface())
+            }
+
+            // if field value is a pointer to struct
+            if fieldVal.Kind()==reflect.Ptr && fieldVal.Type().Elem().Kind() == reflect.Struct {
+                if fieldVal.IsNil() {
+                    fieldVal.Set(reflect.New(fieldVal.Type().Elem()))
+                }
+                // fmt.Printf("recursive: %v %v\n", m,fieldVal.Interface())
+                return FillStruct(m, fieldVal.Interface())
+            }
+
+        }
+
+        return fmt.Errorf("Provided value type didn't match obj field type")
     }
 
-    structFieldValue.Set(val)
+    fieldVal.Set(val)
+    return nil
+
+}
+
+func FillStruct(m map[string]interface{}, s interface{}) error {
+    for k, v := range m {
+        err := SetField(s, k, v)
+        if err != nil {
+            return err
+        }
+    }
     return nil
 }
