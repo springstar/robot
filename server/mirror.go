e5a97cd817fa08bdebaf8762a01661a518c4cdad
wuchunxin <wuyutong0908@gmail.com>
2023-06-27 10:36:38 +0800

    mirror quest

--- server/mirror.go
+++ server/mirror.go
@@ -1,5 +1,19 @@
 package server
 
+import (
+	"github.com/springstar/robot/core"
+	"github.com/springstar/robot/config"
+)
+
 type StageClearQuestData struct {
-	
+
+}
+
+func getStageClearInfo(confQuest config.ConfQuest) (count int, repSn int){
+	infos, err := core.Str2IntSlice(confQuest.Target)
+	if err != nil {
+		core.Error("StageClear quest target error ", confQuest.Sn)
+		return count, repSn
+	}
+	return infos[0], infos[1]
 }
