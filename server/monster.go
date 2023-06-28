a50508aae3f48f0c416677f6f04815ee1b0773aa
wuchunxin <wuyutong0908@gmail.com>
2023-06-28 12:11:55 +0800

    monster quest

--- server/monster.go
+++ server/monster.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/springstar/robot/config"
 	"github.com/springstar/robot/core"
 )
 
@@ -26,3 +27,41 @@ func (d *MonsterQuestData) getQuestSn() int {
 func (d *MonsterQuestData) onStatusUpdate(executor *RobotQuestExecutor, sn int, status QuestStatus) {
 
 }
+
+func (d *MonsterQuestData) genMonsterPosList(confQuest *config.ConfQuest) {
+	monsterSn, err := core.Str2Int(confQuest.ArrParam)
+	if err != nil {
+		core.Error(err)
+		return
+	}
+
+	mapSn, err := core.Str2Int(confQuest.Target)
+	if err != nil {
+		core.Error(err)
+		return
+	}
+
+	confs := config.GetAllConfSceneCharacter()
+	for _, c := range confs {
+		if mapSn != c.SceneID {
+			if len(d.posList) > 0 {
+				break
+			} else {
+				continue
+			}
+		}
+
+		monster, err := core.Str2Int(c.MonsterSn)
+		if err != nil {
+			continue
+		}
+
+		if monster != monsterSn {
+			continue
+		}
+
+		pos := core.Str2Float32Slice(c.Position)
+		d.posList = append(d.posList, core.NewVec2(pos[0], pos[2]))
+	}
+}
+
