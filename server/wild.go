68c03756968e67d53463811ec492e853124a7ce4
wuchunxin <wuyutong0908@gmail.com>
2023-07-20 12:23:06 +0800

    wild executor

--- server/wild.go
+++ server/wild.go
@@ -9,12 +9,16 @@ import (
 
 type WildExecutor struct {
 	*Executor
+	snList []int
 	monsterPosList []*core.Vec2
+	curEnemy int64
+	idx int
 }
 
 func newWildExecutor(r *Robot) *WildExecutor {
 	return &WildExecutor{
 		Executor: newExecutor(r),
+		idx: -1,
 	}
 }
 
@@ -35,6 +39,7 @@ func (q *WildExecutor) genMonsterPosList(mapSn int) {
 			continue
 		}
 
+		q.snList = append(q.snList, confChar.Sn)
 		p := core.Str2Float32Slice(confChar.Position)
 		pos := core.NewVec2(p[0], p[2])
 		q.monsterPosList = append(q.monsterPosList, pos)
@@ -49,14 +54,19 @@ func (q *WildExecutor) exec(params []string, delta int) {
 		return
 	}
 
+	if len(q.snList) == 0 {
+		q.genMonsterPosList(mapSn)
+	}
+
 	if int(q.mapSn) != mapSn {
 		q.switchStage(1, mapSn, mapSn)
 		q.setPause()	
 	}
 
 	if q.getState() == EXEC_PAUSE {	
-		q.genMonsterPosList(mapSn)
 		q.attachCtxFun(asyncWild, q, mapSn)
+	} else {
+		q.execWildFight(mapSn)
 	} 
 }
 
@@ -71,15 +81,63 @@ func asyncWild(e iExecutor, i interface{}) {
 }
 
 func (q *WildExecutor) execWildFight(mapSn int) {
-	n := len(q.monsterPosList)
-	rnd := core.GenRandomInt(n)
-	pos := q.monsterPosList[rnd]
-	ret := q.move(pos)
-	if ret == -1 {
+	if q.curEnemy > 0 {
+		vo := q.findObj(q.curEnemy)
+		if vo == nil {
+			q.curEnemy = 0
+			return
+		}
+
+		curObj := vo.(*MonsterObj)
+		ret := q.move(curObj.pos)
+		if ret == -1 {
+			return
+		}
+
+		q.fight(q.curEnemy)
+		return
+
+	}
+	
+	enemyId := q.lockEnemy()	
+	if enemyId == 0 {
+		// core.Info("monster quest no enemy locked")
 		return
 	}
 
-	
+	q.curEnemy = enemyId
+
+}
+
+func (q *WildExecutor) lockEnemy() int64 {
+	if q.idx == -1 {
+		n := len(q.monsterPosList)
+		q.idx = core.GenRandomInt(n)
+	}
+
+	pos := q.monsterPosList[q.idx]
+	ret := q.move(pos)
+	if ret == -1 {
+		// core.Info("moving to ", pos.X, pos.Y, q.pos.X, q.pos.Y)
+		return 0
+	}
+
+	sn := q.snList[q.idx]
+	enemy := q.findMonsterObj(sn)
+	if enemy == nil {
+		// core.Info("no enemy found ", sn)
+		q.idx = -1
+		return 0
+	}
+
+	if enemy.isDead() {
+		core.Info("enemy is dead ", sn)
+		q.idx = -1
+		return 0
+	}
+
+	return enemy.getId()
+
 }
 
 func (q *WildExecutor) onEvent(k EventKey) {
